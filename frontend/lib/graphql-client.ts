52f5244537ff7eb2e9012cbbb6c0a7024b717377
Abinash321r <abinashnepali619@gmail.com>
2026-09-16 13:12:35 +0545

    updated graphql client

--- frontend/lib/graphql-client.ts
+++ frontend/lib/graphql-client.ts
@@ -1,11 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 
-
-
-
-
-export const gqlClient = new GraphQLClient('http://localhost:3001/graphql', {
+export const gqlClient = new GraphQLClient( `${API_URL}/graphql`, {
   credentials: 'include', 
 });
